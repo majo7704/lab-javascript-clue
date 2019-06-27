07dbff6374e0c8f1e3f41331bbb27a3d83a4a718
j m <jm@js-MacBook-Pro.local>
2019-06-27 14:07:48 +0200

    iteration2

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,94 +1,137 @@
 // Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
+var charactersArray = [];
+var roomsArray = [];
+var weaponsArray = [];
 
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation:   Scientist
 
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
+//CHaracters constructions
+function Characters(first_name, last_name, color, description, age, image, occupation) {
+  this.first_name=first_name;
+  this.last_name=last_name;
+  this.color=color;
+  this.description=description;
+  this.age=age;
+  this.image=image;
+  this.occupation=occupation;
 
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
+  this.addToArray = function(char) { //method to add characters in to array
+    charactersArray.push(char);
+  },
+  this.addToArray(this); //this represents specific character
+  this.randomSelector = function() {
+    return this[Math.floor(Math.random() * this.length)];
+}
+}
 
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
+var mrGreen = new Characters("Jacob", "Green", "green", "He has a lot of connections", "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg", "Entrepreneur")
+  console.log(mrGreen);
+  // mrGreen.addToArray(mrGreen)
+var drOrchid = new Characters("Doctor", "Orchid", "white", "PhD in plant toxicology. Adopted daughter of Mr. Boddy", "http://www.radiotimes.com/uploads/images/Original/111967.jpg", "Scientist")
+var profPlum = new Characters("Victor", "Plum", "purple", "Billionare video game designer", "22", "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg", "Designer")
+var misScarlet = new Characters("Designer", "Scarlet", "red", "She is an A-list movie star with a dark past", "31", "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg", "Actor")
+var mrsPeacock = new Characters("Eleanor", "Peacock", "blue", "She is from a wealthy family and uses her status and money to earn popularity", "36", " https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg", "Socialité")
+var mrMustard = new Characters("Jack", "Mustard", "yellow", "He is a former football player who tries to get by on his former glory", "62", "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg", "Retired Football player")
 
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation:   Retired Football player
+// weapons constructor
+function Weapons(name, weight) {
+  this.name=name;
+  this.weight=weight;
+  this.addToArray = function(weap) {
+    weaponsArray.push(weap);
+  }
+  this.addToArray(this);
+}
+var rope = new Weapons("rope", 10);
+var knife = new Weapons("knife", 8);
+var candlestick = new Weapons("candlestick", 2);
+var dumbbell = new Weapons("dumbbell", 30);
+var poison = new Weapons("poison", 2);
+var axe = new Weapons("axe", 15);
+var bat = new Weapons("bat", 13);
+var trophy = new Weapons("trophy", 25);
+var pistol = new Weapons("pistol", 20);
 
-// Weapons
+//rooms constructor
+function Rooms (name) {
+  this.name=name;
+  this.addToArray = function(room) {
+    roomsArray.push(room);
+  }
+  this.addToArray(this);
+}
+var dinningRoom = new Rooms("Dinning Room");
+var conservatory = new Rooms("Conservatory");
+var kitchen = new Rooms("Kitchen");
+var study = new Rooms("Study");
+var library = new Rooms("Library");
+var billiardRoom = new Rooms("Billiard Room");
+var lounge = new Rooms("Lounge");
+var ballroom = new Rooms("Ballroom");
+var hall = new Rooms("Hall");
+var spa = new Rooms("Spa");
+var livingRoom = new Rooms("Living Room");
+var observatory = new Rooms("Observatory");
+var theater = new Rooms("Theater");
+var guestHouse = new Rooms("Guest House");
+var patio = new Rooms("Patio");
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
 
-// Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
+
+
 
 // Characters Collection
-var charactersArray = [];
-
+// charactersArray.push(dinningRoom)
+console.log(charactersArray)
 // Rooms' Collection
-var roomsArray = [];
-
+console.log(roomsArray)
 // Weapons Collection
-var weaponsArray = [];
+console.log(weaponsArray)
+
+//Random elemnets
+
+
+function Game(charactersArray, weaponsArray, roomsArray){
+
+  this.randomSelector = function (arr) {
+    let randomI = Math.floor(Math.random() * arr.length);
+    return arr[randomI];
+  }
+
+  this.characters = charactersArray;
+  this.weapons = weaponsArray;
+  this.rooms = roomsArray;
+
+  // this.character = this.randomSelector(charactersArray);
+  // this.weapon = this.randomSelector(weaponsArray);
+  // this.room = this.randomSelector(roomsArray);
+
+  this.pickMistery = function(func, array) {
+    let misteryEnvelope = [];
+    misteryEnvelope = [
+      this.randomSelector(this.characters),
+      this.randomSelector(this.weapons),
+      this.randomSelector(this.rooms),
+    ]
+    return misteryEnvelope;
+  }
+
+ this.pickNewMistery = function() {
+   return {
+        character : this.randomSelector(this.characters),
+        weapon : this.randomSelector(this.weapons),
+        room : this.randomSelector(this.rooms)
+    }
+  }
+
+}
+// console.log(Game(charactersArray, weaponsArray, roomsArray));
+var game = new Game(charactersArray, weaponsArray, roomsArray);
+var game2 = new Game(charactersArray, weaponsArray, roomsArray);
+
+console.log(game.pickNewMistery().character);
+
+var mysteryObj = game.pickNewMistery();
+console.log(mysteryObj.character.first_name);
+
+var mysteryArr = game2.pickMistery();
+console.log(mysteryArr[0].first_name)
