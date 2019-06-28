a66e3c8bd81381c38809ff797708e8012e4c73ff
j m <jm@js-MacBook-Pro.local>
2019-06-28 14:37:35 +0200

    DOM added

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -4,7 +4,7 @@ var roomsArray = [];
 var weaponsArray = [];
 
 
-//CHaracters constructions
+//CHaracters constructors
 function Characters(first_name, last_name, color, description, age, image, occupation) {
   this.first_name=first_name;
   this.last_name=last_name;
@@ -23,10 +23,15 @@ function Characters(first_name, last_name, color, description, age, image, occup
 }
 }
 
-var mrGreen = new Characters("Jacob", "Green", "green", "He has a lot of connections", "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg", "Entrepreneur")
-  console.log(mrGreen);
-  // mrGreen.addToArray(mrGreen)
-var drOrchid = new Characters("Doctor", "Orchid", "white", "PhD in plant toxicology. Adopted daughter of Mr. Boddy", "http://www.radiotimes.com/uploads/images/Original/111967.jpg", "Scientist")
+var mrGreen = new Characters(
+  "Jacob",
+  "Green",
+  "green",
+  "He has a lot of connections",
+  22,
+  "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+  "Entrepreneur")
+var drOrchid = new Characters("Doctor", "Orchid", "white", "PhD in plant toxicology. Adopted daughter of Mr. Boddy", 22, "http://www.radiotimes.com/uploads/images/Original/111967.jpg", "Scientist")
 var profPlum = new Characters("Victor", "Plum", "purple", "Billionare video game designer", "22", "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg", "Designer")
 var misScarlet = new Characters("Kasandra", "Scarlet", "red", "She is an A-list movie star with a dark past", "31", "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg", "Actor")
 var mrsPeacock = new Characters("Eleanor", "Peacock", "blue", "She is from a wealthy family and uses her status and money to earn popularity", "36", " https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg", "Socialité")
@@ -41,15 +46,15 @@ function Weapons(name, weight) {
   }
   this.addToArray(this);
 }
-var rope = new Weapons("rope", 10);
-var knife = new Weapons("knife", 8);
-var candlestick = new Weapons("candlestick", 2);
-var dumbbell = new Weapons("dumbbell", 30);
-var poison = new Weapons("poison", 2);
-var axe = new Weapons("axe", 15);
-var bat = new Weapons("bat", 13);
-var trophy = new Weapons("trophy", 25);
-var pistol = new Weapons("pistol", 20);
+var rope = new Weapons("Rope", 10);
+var knife = new Weapons("Knife", 8);
+var candlestick = new Weapons("Candlestick", 2);
+var dumbbell = new Weapons("Dumbbell", 30);
+var poison = new Weapons("Poison", 2);
+var axe = new Weapons("Axe", 15);
+var bat = new Weapons("Bat", 13);
+var trophy = new Weapons("Trophy", 25);
+var pistol = new Weapons("Pistol", 20);
 
 //rooms constructor
 function Rooms (name) {
@@ -81,11 +86,11 @@ var patio = new Rooms("Patio");
 
 // Characters Collection
 // charactersArray.push(dinningRoom)
-console.log(charactersArray)
-// Rooms' Collection
-console.log(roomsArray)
-// Weapons Collection
-console.log(weaponsArray)
+// console.log(charactersArray)
+// // Rooms' Collection
+// console.log(roomsArray)
+// // Weapons Collection
+// console.log(weaponsArray)
 
 //Random elemnets
 
@@ -99,7 +104,7 @@ function Game(charactersArray, weaponsArray, roomsArray){
   this.characters = charactersArray;
   this.weapons = weaponsArray;
   this.rooms = roomsArray;
-  this.misteryEnvelope = []
+  this.misteryEnvelope = {}
   // this.character = this.randomSelector(charactersArray);
   // this.weapon = this.randomSelector(weaponsArray);
   // this.room = this.randomSelector(roomsArray);
@@ -115,28 +120,66 @@ function Game(charactersArray, weaponsArray, roomsArray){
     this.misteryEnvelope = mistery
     return mistery
   }
-  this.revealMistery = function (array) {
+
+  this.revealMistery = function () {
     // let misteryEnvelope = this.misteryEnvelope;
+    var murderer = document.getElementById("murderer");
+    murderer.setAttribute("src", this.misteryEnvelope.character.image)
+    var tool = document.getElementById("weapon");
+    tool.innerHTML = this.misteryEnvelope.weapon.name;
+    var place = document.getElementById("rooms");
+   
+    place.innerHTML = this.misteryEnvelope.room.name;
     return this.misteryEnvelope.character.first_name + " " + this.misteryEnvelope.character.last_name + " killed Mr.Smith using the " + this.misteryEnvelope.weapon.name + " in the " + this.misteryEnvelope.room.name + "!!!!";
+    
     };
 }
 
-// console.log(Game(charactersArray, weaponsArray, roomsArray));
+//construct a new game object.
 var game = new Game(charactersArray, weaponsArray, roomsArray);
 
 
-console.log(game.pickMistery().character);
-console.log(game.pickMistery().weapon);
-console.log(game.pickMistery().room);
+setTimeout( function(){
+  //initialise game ( create misteryEnvelope with random objects);
+  game.pickMistery();
+  game.revealMistery();
 
+}, 3000 );
 
+// this ==> window;
 
-console.log(game.revealMistery());
+//Getting the button
+var playButton = document.getElementById("play")
+
+//adding eventlistener
+playButton.addEventListener("click", function( event ){
+
+  //initialise game ( create misteryEnvelope with random objects );
+  game.pickMistery();
+
+  this.style.backgroundColor = "red"
+  //console.log("the element ", this );
+  //console.log("the event", event);
+
+  game.revealMistery();
+
+  this.innerHTML = "Start new mistery"
+
+})
 
 
 //var misteryObj = game.pickMistery();
 //console.log(misteryObj.character.first_name);
 
+//function randomSelector(someArray) {
+  //return someArray[Math.floor(Math.random() * someArray.length)]
+//}
+//let misteryEnvelope = {
+  //murderer: {},
+  //room: {},
+  //weapon: {}
+//}
 
-
-
+//misteryEnvelope.murderer = randomSelector(characters);
+//misteryEnvelope.room = randomSelector(rooms)
+//misteryEnvelope.weapon = randomSelector(weapon)
